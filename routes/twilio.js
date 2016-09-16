4d31412f916b87e7d3603ae1a65e06e421158864
Christie R <creindle@hawaii.edu>
2016-09-15 18:40:54 -1000

    Need to add twilio pictures into database

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,29 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
 var db = require('../models');
-
 var Refferals = db.Refferals;
 
-// app.post("/message", function (request, response) {
-//   //console.log(request.body);
-//   response.send("<Response><Message>Thank you for your referral</Message></Response>")
-// });
-
 app.post('/message', function(req, res) {
-  console.log(req.body);
-    // Inserts Location data to  Locations table
-    Refferals.create({refferalStatus:1,
-                      phoneNumber: req.body.From,
-                      city: req.body.FromCity,
-                      state: req.body.FromState,
-                      zip: req.body.FromZip,
-                      description: req.body.Body})
-   .then(function(refferal) {
-     // Sends response that tells the pic got uploaded
-    res.send("<Response><Message>Thank you for your referral</Message></Response>")
-   })
+  return Refferals
+    .create(
+      {
+        refferalStatus: 1,
+        phoneNumber: req.body.From,
+        description: req.body.Body
+      }
+    )
+    .then(function(refferal) {
+      res.send("<Response><Message>Thank you for your referral</Message></Response>")
+    });
 });
 
 module.exports = app;
