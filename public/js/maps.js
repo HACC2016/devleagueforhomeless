ac00d8e1cae129eda80b2bb3a4e29da278a7c765
kekuso <kekuso808@gmail.com>
2016-09-18 10:29:59 -1000

    Minor changes to full description

--- public/js/maps.js
+++ public/js/maps.js
@@ -38,7 +38,7 @@ function initMap(){
   function populateInfoWindow(marker, infowindow) {
     if (infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<div class="marker">' + 'Ticket ID: ' + marker.title + '</div>');
+      infowindow.setContent('<div class="marker">' + 'Referral #: ' + marker.title + '</div>');
       infowindow.open(map, marker);
       infowindow.addListener('closeclick', function() {
         infowindow.marker = null;
