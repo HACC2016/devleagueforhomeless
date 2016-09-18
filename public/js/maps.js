64f365d3b181c8f595f6408008d9d7c89cd691ac
sgalutira <sheena.galutira@gmail.com>
2016-09-17 20:35:54 -1000

    edit bounds

--- public/js/maps.js
+++ public/js/maps.js
@@ -47,12 +47,12 @@ function initMap(){
   }
 
       function showListings(){
-        var bounds = new google.maps.LatLngBounds();
+        // var bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < markers.length; i++){
           markers[i].setMap(map);
-          bounds.extend(markers[i].position);
+          // bounds.extend(markers[i].position);
         }
-        map.fitBounds(bounds);
+        // map.fitBounds(bounds);
       }
 
       function hideListings(){
