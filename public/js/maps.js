3b4f03500a35cfb7cc3fe01eac3a18b5361910bd
sgalutira <sheena.galutira@gmail.com>
2016-09-21 19:36:52 -1000

    Fixed map to get address without geolocation and geolocation

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,37 +1,69 @@
+var markers = [];
+var color1 = 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png';
+var color2 = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+
 function initMap(){
-  var markers = [];
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 21.3069, lng: -157.8583},
     zoom: 10
   });
 
+  var geocoder = new google.maps.Geocoder();
+
   $.ajax({url: '/homeless', success: (function (result){
     var referrals = result;
     referrals.map(function(loc){
+      var address = loc.address + ' ' + loc.city + ', ' + loc.state + ' ' + loc.zip;
+      console.log('address: ', address);
       var coordinatesLat = loc.latitude;
       var coordinatesLng = loc.longitude;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: {
-          lat: parseFloat(coordinatesLat),
-          lng: parseFloat(coordinatesLng),
-        },
-        animation: google.maps.Animation.DROP,
-        title: loc.id.toString(),
-        icon: 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'
-      });
-      markers.push(marker);
-      var largeInfowindow = new google.maps.InfoWindow();
-      marker.addListener('click', function(){
-        populateInfoWindow(marker, largeInfowindow);
-      });
 
-      document.getElementById('show-listings').addEventListener('click', showListings);
+      if (geocoder) {
+        geocoder.geocode({
+          'address': address
+        }, function (results, status){
+          if (status === google.maps.GeocoderStatus.OK){
+            if (status !== google.maps.GeocoderStatus.ZERO_RESULTS){
+              map.setCenter(results[0].geometry.location);
+              console.log(  {
+                  lat: parseFloat(coordinatesLat),
+                  lng: parseFloat(coordinatesLng),
+                });
+              console.log('results[0]', results[0].geometry.location);
+              var marker = new google.maps.Marker({
+                map: map,
+                position: coordinatesLat && coordinatesLng ?
+                {
+                  lat: parseFloat(coordinatesLat),
+                  lng: parseFloat(coordinatesLng),
+                } : results[0].geometry.location,
+                animation: google.maps.Animation.DROP,
+                title: loc.id.toString(),
+                icon: color1,
+              });
 
-      document.getElementById('hide-listings').addEventListener('click', hideListings);
+              markers.push(marker);
 
+              google.maps.event.addListener(marker, 'mouseover', function() {
+                marker.setIcon(color2);
+              });
+              google.maps.event.addListener(marker, 'mouseout', function() {
+                marker.setIcon(color1);
+              });
 
-    });
+              var largeInfowindow = new google.maps.InfoWindow();
+              marker.addListener('click', function(){
+                populateInfoWindow(marker, largeInfowindow);
+              });
+
+            }
+          }
+        }); // close geocoder callback
+      } // close geocoder if statement
+      }); //closes referrals.map function
+        document.getElementById('show-listings').addEventListener('click', showListings);
+
+        document.getElementById('hide-listings').addEventListener('click', hideListings);
   }) //close success function
 }); //close ajax
 
